c2538f1662a2c05fb5044b432de3a4f5c23641c3
FernandaFigueirdo <fernandacalfig@tecgraf.puc-rio.br>
2024-08-23 16:08:40 -0300

    modified:   scripts/scriptPag3.js 	modified:   scripts/scriptPag4.js 	modified:   scripts/scriptPag5.js

--- scripts/scriptPag3.js
+++ scripts/scriptPag3.js
@@ -10,12 +10,36 @@ function limpar(){
 
 }
 
+function validarExtensao(arquivo,extensao){
+
+    caminho = arquivo.value.split("\\");
+
+    alert("Primeira forma: "+
+        caminho[caminho.length-1])
+
+        let indice = caminho.length-1;
+        let comp = caminho[indice].split(".");
+        let ext = comp[comp.length-1];
+   
+        if(extensao.lower === ext.lower){
+           alert("Igual");
+        }else{
+           alert("Diferente");
+        }
+   }
+
+
+
 window.addEventListener("load", function(){
     let btnLimpar = document.querySelector('#btnLimpa');
+    let btnSubmeter = document.querySelector("btnSubmete")
 
     btnLimpar.addEventListener("click", function(){
             limpar();
     });
+    btnSubmeter.addEventListener("click", function(){
+            validarExtensao()
+    })
 
 
 });
